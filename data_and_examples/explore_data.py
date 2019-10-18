f7a9dd79f8ccd01ce219efffcb667ddcc21d7c76
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-10-18 16:38:46 +0200

    data aggregator script

---
+++ data_and_examples/explore_data.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import os
+import pandas as pd
+from tqdm import tqdm
+
+# Get all data files
+DATA_FILE_PATH = "./example_data"
+PROCESSED_DATA_FILE_PATH = "./processed_data"
+LEGS_FILENAME = "legs.csv"
+LEGS_FILEPATH = DATA_FILE_PATH + "/" + LEGS_FILENAME
+
+leg_df = pd.read_csv(LEGS_FILEPATH, index_col=0)
+
+# ask which user we want to select (default is all)
+user_ids = leg_df.user.unique()
+print("Please select a user:")
+print(0, ":",  "all")
+for idx, user in enumerate(user_ids):
+    print(idx + 1, ":",  user)
+
+user_id_idx = int(input("User idx (default is 0): ") or 0) - 1
+
+# If all, then user_ids contains all the user ids, otherwise just the one we want
+if user_id_idx < 0:
+    print ("Aggregating data for all users")
+else:
+    print ("Selected user", user_ids[user_id_idx])
+    user_ids = [user_ids[user_id_idx]]
+
+selected_ids = {}
+# For every selected user
+for user_id in user_ids:
+    print("Please select a leg (just one of the first 10):")
+    print(0, ":",  "all")
+    leg_ids = leg_df.loc[leg_df.user == user_id].id
+    for idx, leg_id in enumerate(leg_ids[0:10]):
+        print(idx + 1, ":",  leg_id)
+
+    leg_id_idx = int(input("Leg idx (default is 0): ") or 0) - 1
+    if leg_id_idx < 0:
+        selected_ids[user_id] = leg_ids
+    else:
+        selected_ids[user_id] = [leg_ids[leg_id_idx]]
+
+# Init data files
+data_dataframes = []
+print("Loading sensor csvs")
+for file in tqdm(os.listdir(DATA_FILE_PATH)):
+    if not file == LEGS_FILENAME:
+         data_dataframes.append((file, pd.read_csv(DATA_FILE_PATH + '/' + file)))
+
+print("")
+
+# Create folder for processed data
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+create_folder(PROCESSED_DATA_FILE_PATH)
+# For every user, for every selected leg, for every datafile
+for user_id in user_ids:
+    # Create user directory in processed
+    user_fp = PROCESSED_DATA_FILE_PATH + "/user_" + user_id
+    create_folder(user_fp)
+    print("Processing legs for user ", user_id)
+    for leg_id in tqdm(selected_ids[user_id]):
+        # Create leg directory in processed
+        leg_fp = user_fp + "/leg_" + str(leg_id).split(".")[0]
+        create_folder(leg_fp)
+        for data_df in data_dataframes:
+            # Create csv file with all sensor readings of the user's trip
+            leg_sensor_fp = leg_fp + "/" + data_df[0];
+            df = data_df[1]
+            leg_sensor_data = df.loc[(df.user == user_id) & (df.leg == leg_id)].copy()
+            leg_sensor_data.to_csv(leg_sensor_fp)
