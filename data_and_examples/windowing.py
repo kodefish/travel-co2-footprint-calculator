da3bd76832530d4fa168701b80a20c9f1199ad43
vhelium <privat@vhelium.com>
2019-11-01 17:38:00 +0100

    working on windowing. see previous commit

--- data_and_examples/windowing.py
+++ data_and_examples/windowing.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
 import os
 import pandas as pd
+from tqdm import tqdm
 
 # data files
 DATA_FILE_PATH = "./example_data"
@@ -46,17 +47,56 @@ else:
     print ("Selected user", user_ids[user_id_idx])
     user_ids = [user_ids[user_id_idx]]
 
+# helper functions that:
+#   Given a DataFrame of the corresponding sensor
+#   Does aggregate (in window_size blocks) the data and stores it in the global Data Frames for the given transport!
+
+def parseAcc(df, tmode):
+    # iterate over all columns..
+    # while `time passed` < `window_size`
+    #   aggregate that shit (min, max, median, ...)
+    # save that window as a new row into the global DataFrame for that mode (together with the other sensors --> HOW!?)
+    print ("todo..")
+
+def parseBT(df, tmode):
+    print ("todo..")
+
+
+def parseGyro(df, tmode):
+    print ("todo..")
+
+
+def parseLoc(df, tmode):
+    print ("todo..")
+
+
+def parseMagn(df, tmode):
+    print ("todo..")
+
+
+def parseWifi(df, tmode):
+    print ("todo..")
+
 # for every selected user, go through all processed legs
 selected_ids = {}
 for user_id in user_ids:
     user_fp = PROCESSED_DATA_FILE_PATH + "/user_" + user_id
-    for file in os.listdir(user_fp):
+    for file in tqdm(os.listdir(user_fp)):
         leg_id = int(file[4:])
         # totally-not-hacky mode extraction:
         raw_tmode = int(leg_df.loc[(leg_df.user == user_id) & (leg_df.id == leg_id)].iloc[0]["mode"])
         tmode = tmode_map[raw_tmode]
 
         print("Parsing leg [id=" + str(leg_id) + ", mode=" + str(tmode) + "]")
+
+        cur_fp = user_fp + "/" + file
+
+        parseAcc(pd.read_csv(cur_fp + "/acc_readings.csv"), tmode)
+        parseBT(pd.read_csv(cur_fp + "/bluetooth_scans.csv"), tmode)
+        parseGyro(pd.read_csv(cur_fp + "/gyro_readings.csv"), tmode)
+        parseLoc(pd.read_csv(cur_fp + "/locations_scans.csv"), tmode)
+        parseMagn(pd.read_csv(cur_fp + "/magn_readings.csv"), tmode)
+        parseWifi(pd.read_csv(cur_fp + "/wifi_scans.csv"), tmode)
 
         # TODO:
         # go through all csvs of that leg w/ the given mode..
@@ -66,3 +106,5 @@ for user_id in user_ids:
             # save all aggregated windows in the DataFrame for that transport mode
             # s.t. in the end we have one DataFrame per mode containing all windows we have for a given mode
             # (independent of the user or actual leg)
+
+
