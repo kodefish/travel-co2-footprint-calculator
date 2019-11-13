0be06684a540600e1d860d21f01c7306e385246a
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2019-11-13 13:19:09 +0100

    Added 2-3 lines to Trip, and the e-bike EF

--- smartenergy/app/src/main/java/ch/ethz/smartenergy/footprint/Footprint.java
+++ smartenergy/app/src/main/java/ch/ethz/smartenergy/footprint/Footprint.java
@@ -8,8 +8,8 @@ public class Footprint {
      * Data comes from:
      * Car: https://www.eea.europa.eu/highlights/average-co2-emissions-from-new
      * Bike: https://ecf.com/news-and-events/news/how-much-co2-does-cycling-really-save
-     * E-Bikes: TODO
-     * Motorcycles: TODO
+     * E-Bikes: Same as bikes (for lack of additional data
+     * Motorcycles: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=10&cad=rja&uact=8&ved=2ahUKEwjswZ_d6eblAhXWw8QBHdSzAuIQFjAJegQIBBAC&url=https%3A%2F%2Fwww.lowcvp.org.uk%2Fassets%2Fpresentations%2FMCI%2520-%2520Greg%2520Archer.pdf&usg=AOvVaw3jXJ9ZdEBkZ0JHN3ikOLXE
      * Everything else: https://jamesrivertrans.com/wp-content/uploads/2012/05/ComparativeEnergy.pdf
      *
      * @param tripType the type of transport
@@ -22,9 +22,9 @@ public class Footprint {
             case BUS: return 144.72;
             case CAR: return 123.4;
             case TRAM: return 125.47;
+            case EBIKE: // Same as bike
             case BIKE: return 21;
-            case EBIKE: return 0; // TODO
-            case MOTORCYCLE: return 0; // TODO
+            case MOTORCYCLE: return 110;
             default: return -1; // Should not happen, but default is needed
         }
     }
