b1492f59895f68f9baade4c3c5c24c1452dfb12b
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2019-11-20 16:28:37 +0100

    Worked on Trip, and TripAdapter

--- android-app/app/src/main/java/ch/ethz/smartenergy/footprint/Trip.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/footprint/Trip.java
@@ -1,23 +1,24 @@
 package ch.ethz.smartenergy.footprint;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Date;
 import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
 
 public class Trip {
     List<Leg> legs;
     Double totalFootprint = null;
     Double totalDistance = null;
-    List<TripType> modesUsed;
+    Map<TripType, Integer> modesUsed;
+    List<TripType> modesUsedDescOrder = null;
+    Integer numModesUsed = null;
     Date date;
-
-    /**
-     * Constructs a trip with no legs
-     */
-    public Trip() {
-        legs = new ArrayList<Leg>();
-    }
 
     /**
      * Constructs a trip with one or more legs
@@ -25,36 +26,12 @@ public class Trip {
      */
     public Trip(List<Leg> legs) {
         this.legs = new ArrayList<>(legs);
-        this.modesUsed = new ArrayList<TripType>();
-        for (Leg leg: legs) {
-            if (!modesUsed.contains(leg.getMostProbableLegType())) {
-                modesUsed.add(leg.getMostProbableLegType());
-            }
-        }
-
+        this.modesUsed = new HashMap<TripType, Integer>();
         this.date = Calendar.getInstance().getTime();
-    }
 
-    /**
-     * Constructs a trip with one or more legs
-     * @param legs the legs to add to the trip
-     */
-    public Trip(Leg... legs) {
-        this();
-        this.addLegs(legs);
-    }
-
-    /**
-     * Adds one or more legs to the trip
-     * @param legs the legs to add to the trip
-     */
-    public void addLegs(Leg... legs) {
         for (Leg leg: legs) {
-            this.legs.add(leg);
-
-            if (!modesUsed.contains(leg.getMostProbableLegType())) {
-                modesUsed.add(leg.getMostProbableLegType());
-            }
+            TripType mode = leg.getMostProbableLegType();
+            this.modesUsed.put(mode, modesUsed.get(mode) + 1);
         }
     }
 
@@ -93,11 +70,69 @@ public class Trip {
     }
 
     /**
-     * Returns the list of transportation modes used
+     * Returns the number of transportation modes used
+     * @return the number of transportation modes used
+     */
+    public int getNumModesUsed() {
+        if (numModesUsed == null) {
+            numModesUsed = 0;
+            for (Leg leg: this.legs) {
+                if (this.modesUsed.get(leg.getMostProbableLegType()) != 0) {
+                    numModesUsed++;
+                }
+            }
+        }
+        return numModesUsed;
+    }
+
+    /**
+     * Returns the list of transportation modes used (by descending order)
      * @return the list of transportation modes used
      */
     public List<TripType> getModesUsed() {
-        return (List<TripType>) ((ArrayList<TripType>) this.modesUsed).clone();
+        if (modesUsedDescOrder == null) {
+            // Getting the entrySet
+            Set<Map.Entry<TripType, Integer>> numUsedSet = this.modesUsed.entrySet();
+            // Converting HashMap to List of Map entries
+            List<Map.Entry<TripType, Integer>> numUsedListEntry = new ArrayList<Map.Entry<TripType, Integer>>(numUsedSet);
+
+            // Sorting by descending use
+            Collections.sort(numUsedListEntry,
+                    new Comparator<Map.Entry<TripType, Integer>>() {
+
+                        @Override
+                        public int compare(Map.Entry<TripType, Integer> mode1,
+                                           Map.Entry<TripType, Integer> mode2) {
+                            return mode2.getValue().compareTo(mode1.getValue());
+                        }
+                    });
+
+            // Storing
+            List<TripType> asList = new ArrayList<TripType>();
+            for (Map.Entry<TripType, Integer> map : numUsedListEntry) {
+                asList.add(map.getKey());
+            }
+
+            this.modesUsedDescOrder = asList;
+        }
+        return this.modesUsedDescOrder;
+    }
+
+    /**
+     * Returns a string representation of the transportation modes used (by descending order)
+     * @return a string representation of the transportation modes used
+     */
+    public String getModesAsString() {
+        StringBuilder sb = new StringBuilder();
+        for (TripType mode: this.modesUsedDescOrder) {
+            sb.append(mode.toString().toLowerCase()).append(", ");
+        }
+
+        // Removing the last ", "
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
     }
 
     /**
@@ -106,5 +141,14 @@ public class Trip {
      */
     public Date getDate() {
         return this.date;
+    }
+
+    /**
+     * Returns a dd-MM-yyyy string representation of this trip's date
+     * @return a dd-MM-yyyy string representation of this trip's date
+     */
+    public String getDateAsString() {
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        return df.format(this.date);
     }
 }
--- android-app/app/src/main/java/ch/ethz/smartenergy/ui/HistoryFragment.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/ui/HistoryFragment.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,81 +18,38 @@ import ch.ethz.smartenergy.R;
 
 import ch.ethz.smartenergy.footprint.*;
 import ch.ethz.smartenergy.persistence.TripStorage;
+import ch.ethz.smartenergy.ui.adapters.TripAdapter;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-// Container for information about a trip
-class TripInfo {
-    protected double distance;
-    protected List<TripType> modesUsed;
-    protected double footprint;
-    protected Date date;
-
-    public TripInfo(Trip t) {
-        this.distance = t.getTotalDistance();
-        this.modesUsed = t.getModesUsed();
-        this.footprint = t.getTotalFootprint();
-        this.date = t.getDate();
-    }
-}
-
 public class HistoryFragment extends Fragment {
 
     private List<Trip> pastTrips;
-    private List<TripInfo> tripsInfo;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_history, container, false);
         final TextView textView = root.findViewById(R.id.text_history);
 
-        // TODO: why is that not working ?
-        TripStorage storage = TripStorage(this.getApplicationContext());
+        TripStorage storage = new TripStorage(getContext());
         try {
             pastTrips = storage.getAllStoredTrips();
         } catch (FileNotFoundException e) {
             // Do nothing
         }
 
-        for (Trip t: pastTrips) {
-            tripsInfo.add(new TripInfo(t));
-        }
+        final TripAdapter adapter = new TripAdapter(getContext(),-1, pastTrips);
+        ListView listView = root.findViewById(R.id.history_list_view);
+        listView.setAdapter(adapter);
 
-        populateList();
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            // TODO: stuff
+        });
 
         return root;
-    }
-
-    // TODO: This is supposed to write all the information about the trips' informations on the list of past trips
-    // TODO: Right now, all it does is put those informations in variables
-    // TODO: I don't know how to made it work
-    public void populateList() {
-        for (TripInfo info: tripsInfo) {
-            String date = dateToString(info.date);
-            double distance = info.distance;
-            double footprint = info.footprint;
-            String modesOfTransport = modesToString(info.modesUsed);
-        }
-    }
-
-    private String dateToString(Date d) {
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        return df.format(d);
-    }
-
-    private String modesToString(List<TripType> modes) {
-        StringBuilder sb = new StringBuilder();
-        for (TripType mode: modes) {
-            sb.append(mode.toString().toLowerCase());
-        }
-
-        // Removing the last ", "
-        sb.deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(sb.length() - 1);
-
-        return sb.toString();
     }
 }
