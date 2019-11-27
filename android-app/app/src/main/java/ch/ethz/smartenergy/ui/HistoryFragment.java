ff274e9051fa3b5a81abd8b18cdf3f57f048b3ce
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-11-27 20:01:00 +0100

    removed some imports

--- android-app/app/src/main/java/ch/ethz/smartenergy/ui/HistoryFragment.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/ui/HistoryFragment.java
@@ -6,20 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-
-import android.content.Context;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.FileNotFoundException;
 import java.util.List;
 
 import ch.ethz.smartenergy.R;
@@ -27,15 +17,8 @@ import ch.ethz.smartenergy.TripSummaryActivity;
 import ch.ethz.smartenergy.footprint.Trip;
 import ch.ethz.smartenergy.persistence.TripStorage;
 
-import ch.ethz.smartenergy.footprint.*;
-import ch.ethz.smartenergy.persistence.TripStorage;
 import ch.ethz.smartenergy.ui.adapters.TripAdapter;
 
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class HistoryFragment extends Fragment {
 
