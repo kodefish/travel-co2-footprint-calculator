b14959df222421079e41701aa5ed08668ae5c595
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-05 13:05:12 +0100

    minor refactor

--- android-app/app/src/main/java/ch/ethz/smartenergy/MainActivity.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/MainActivity.java
@@ -2,11 +2,6 @@ package ch.ethz.smartenergy;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
