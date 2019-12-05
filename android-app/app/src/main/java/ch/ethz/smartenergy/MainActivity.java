289d130d89acc786012c52a5be0b8f4ad78fbce5
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-05 11:44:39 +0100

    only home fragment is top level destination

--- android-app/app/src/main/java/ch/ethz/smartenergy/MainActivity.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_history, R.id.nav_history)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
