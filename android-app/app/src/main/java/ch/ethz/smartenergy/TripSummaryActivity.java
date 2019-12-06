cec59b0b1cffd4f1bb62a89c3137411516d839ef
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-06 18:05:55 +0100

    fixed weird map crash bug on first launch

--- android-app/app/src/main/java/ch/ethz/smartenergy/TripSummaryActivity.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/TripSummaryActivity.java
@@ -39,7 +39,7 @@ import ch.ethz.smartenergy.persistence.TripStorage;
 import ch.ethz.smartenergy.ui.adapters.LegAdapter;
 import ch.ethz.smartenergy.ui.util.OnItemClickListener;
 
-public class TripSummaryActivity extends FragmentActivity implements OnMapReadyCallback {
+public class TripSummaryActivity extends FragmentActivity {
 
     public static final String EXTRA_TRIP_ID = "extra_trip_id";
     private static final int MAP_BOUNDS_PADDING_AMOUNT = 375;
@@ -142,19 +142,9 @@ public class TripSummaryActivity extends FragmentActivity implements OnMapReadyC
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(googleMap -> googleMap.setOnMapLoadedCallback(() -> onMapReady(googleMap)));
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
