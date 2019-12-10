fe445e141afae26e74137b78026b3d721b50d8a2
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-10 18:39:54 +0100

    changed app icon and name

--- android-app/app/src/main/java/ch/ethz/smartenergy/service/DataCollectionService.java
+++ android-app/app/src/main/java/ch/ethz/smartenergy/service/DataCollectionService.java
@@ -133,7 +133,7 @@ public class DataCollectionService extends Service {
     }
 
     private void showNotification() {
-        int icon = R.drawable.ic_launcher_foreground;
+        int icon = R.drawable.ic_stat;
         String text = getString(R.string.notification_service_text);
 
         // Create mandatory notification channel
