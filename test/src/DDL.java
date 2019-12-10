3807b2190b9b29cbc62c8272ec9999e2a2728475
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-10 16:00:12 +0100

    added app screen shots, removed old app and test of fft java impl

--- test/src/DDL.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class DDL {
-    public ArrayList<Double> a, b;
-    public DDL(ArrayList<Double> a, ArrayList<Double> b) {
-        this.a = a;
-        this.b = b;
-    }
-}
